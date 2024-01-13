e9cd2a09c2593d04be8df623f4b888205f464324
malvaceae <malvaceae@hotmail.com>
2024-01-14 02:20:00 +0900

    update layout min height

--- src/app.vue
+++ src/app.vue
@@ -158,7 +158,7 @@ const resize = (size: { width: number, height: number }) => {
 </script>
 
 <template>
-  <q-layout view="hHh LpR fFf">
+  <q-layout view="hHh LpR fFf" :style="{ minHeight: `calc(var(--visual-viewport-height))` }">
     <q-header class="bg-grey-9 text-grey-3" elevated>
       <q-toolbar class="q-mx-auto">
         <q-toolbar-title shrink>
